b9e743581775262fb7c106cd1e23b24dead62acc
Patrick <31795545+Bug-Patrick@users.noreply.github.com>
2020-04-21 10:34:48 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+  
+from distutils.core import setup
+
+setup(name='CE_Bot',
+      version='1.0',
+      description='Bot for CE Discord Server',
+      author='Niklas Beck and Paul Hollmann',
+      author_email='',
+      url='',
+      packages=[],
+      
