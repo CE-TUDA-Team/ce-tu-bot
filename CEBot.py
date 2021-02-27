ed5f7e7c982e54d05b001a2a75a010f3407bf2ae
Niklas <nik-beck@web.de>
2021-02-27 01:13:51 +0100

    checking if bot is on heroku or locally

--- CEBot.py
+++ CEBot.py
@@ -14,22 +14,21 @@ client.description = "Bot for the CE Discord Server"
 async def on_ready():
     global ce_server
     print("Der CE-Bot {0.user} hat sich eingeloggt. ~Niklas".format(client))
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="for commands"))
     # control message for admins at log in
     ce_server = get(client.guilds, name="Computational Engineering")
     admin_channel = get(ce_server.channels, name="logs")
-    await admin_channel.send("Bot ist online")
-    for i in range(1, 5):
-        if get(ce_server.roles, name="Gast_Sem"+str(i)) is not None:
-            temp_role = get(ce_server.roles, name="Gast_Sem"+str(i))
-            print("Die temporäre Rolle " + str(temp_role) + " wird von allen Mitgliedern entfernt.")
-            for member in temp_role.members:
-                await member.remove_roles(temp_role)
-    if get(ce_server.roles, name="Gast_Sem5+") is not None:
-        temp_role = get(ce_server.roles, name="Gast_Sem5+")
-        print("Die temporäre Rolle " + str(temp_role) + " wird von allen Mitgliedern entfernt.")
-        for member in temp_role.members:
-            await member.remove_roles(temp_role)
+    if is_on_heroku():
+        await admin_channel.send("Bot ist online")
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="for commands"))
+        for i in ["1", "2", "3", "4", "5+"]:
+            if get(ce_server.roles, name=f"Gast_Sem{i}") is not None:
+                temp_role = get(ce_server.roles, name=f"Gast_Sem{i}")
+                print("Die temporäre Rolle " + str(temp_role) + " wird von allen Mitgliedern entfernt.")
+                for member in temp_role.members:
+                    await member.remove_roles(temp_role)
+    else:
+        # await admin_channel.send("Bot in development")
+        await client.change_presence(status=discord.Status.dnd)
 
 
 @client.event
@@ -78,6 +77,20 @@ async def load(ctx, extension):
     else:
         await ctx.send(f'{extension} was loaded!')
         client.load_extension(f'Cogs.{extension}')
+
+
+def is_on_heroku():
+    py_path = os.environ['PYTHONPATH']
+    print("Python at Path " + py_path + " used.")
+    if str(py_path) == '/app':
+        return True
+    else:
+        return False
+
+
+"""
+Environment variables for heroku: environ({'PYTHONUNBUFFERED': 'true', 'LD_LIBRARY_PATH': '/app/.heroku/vendor/lib:/app/.heroku/python/lib:', 'PYTHONHOME': '/app/.heroku/python', 'LANG': 'en_US.UTF-8', 'DYNO': 'run.5497', 'PYTHONHASHSEED': 'random', 'FORWARDED_ALLOW_IPS': '*', 'PWD': '/app', 'DYNO_RAM': '512', 'HOME': '/app', 'PORT': '18718', 'LIBRARY_PATH': '/app/.heroku/vendor/lib:/app/.heroku/python/lib:', 'GUNICORN_CMD_ARGS': '--access-logfile -', 'TERM': 'xterm-256color', 'WEB_CONCURRENCY': '2', 'SHLVL': '1', 'PYTHONPATH': '/app', 'PATH': '/app/.heroku/python/bin:/usr/local/bin:/usr/bin:/bin', 'PS1': '\\[\\033[01;34m\\]\\w\\[\\033[00m\\] \\[\\033[01;32m\\]$ \\[\\033[00m\\]', '_': '/app/.heroku/python/bin/python'})
+"""
 
 
 for filename in os.listdir('./Cogs'):
