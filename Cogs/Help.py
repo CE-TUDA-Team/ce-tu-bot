a3c2f252cc27f82d33b15860b321c3ad4f6c4715
Niklas <nik-beck@web.de>
2021-06-28 12:17:40 +0200

    Revert "Revert "Embeds (Help, Custom) und Alias for Sem""
    This reverts commit dc6c9ec22ebc4c07bca6a8809491cd875f2f131f.

--- Cogs/Help.py
+++ Cogs/Help.py
@@ -1,7 +1,10 @@
-from discord import Embed
+import os
+
+import discord
 from discord.ext import commands
 
 
+# TODO Spamschutz und ähnliches
 class Help(commands.Cog):
 
     def __init__(self, client):
@@ -13,8 +16,57 @@ class Help(commands.Cog):
         return
 
     @commands.command()
+    @commands.has_role('Team')
+    async def hilfe_embed(self, ctx, channel: discord.TextChannel = None):
+        embed = discord.Embed(title="CE Bot Befehle",
+                              description="Aufzählung der Befehle unseres CE Bots",
+                              color=discord.Color.blue())
+        embed.add_field(name="Erklärung",
+                        value="Der CE Bot verteilt automatisiert Rollen, sodass du nicht eine Vielzahl an Kanälen "
+                              "siehst und besser die Übersicht behalten kannst." + os.linesep +
+                              "Auf der linken Seite sind Befehle in Anführungszeichen aufgeführt, "
+                              "diese bestehen teilweise aus mehreren durch Lehrzeichen getrennten "
+                              "Zeilen und variablen Zahlen. "
+                              "Links des Gedankenstrichs ist die Erklärung für den Befehl.",
+                        inline=False)
+        embed.add_field(name="Befehle für Rollen für neuen Studenten",
+                        value="'$ersti'  -  Rolle für Bachelor Erstsemestler" + os.linesep +
+                              "'$ersti Master'  -  Rolle für Master & Erstsemestler",
+                        inline=False)
+        embed.add_field(name="Befehle für Rollen des Bachelor-Semesters",
+                        value="'$semester 1'  -  Erstes Semester" + os.linesep +
+                              "'$semester n'  -  n.tes Semester" + os.linesep +
+                              "'$sem n'  -  n.tes Semester",
+                        inline=False)
+        embed.add_field(name="Befehle für Rollenveränderungen im Bachelor",
+                        value="'$nextsem'  -  Rolle für das nächste Semester" + os.linesep +
+                              "'$noersti'  -  Entfernen der Ersti Rolle",
+                        inline=False)
+        embed.add_field(name="Befehle für Rollen für Master Studenten",
+                        value="'$master'  -  Rolle für Master Studenten",
+                        inline=False)
+        embed.add_field(name="Befehle für Rollen zu Vertiefungsrichtungen",
+                        value="'$vertiefung BI'  -  Rolle für Vertiefung Bauingenieurwesen" + os.linesep +
+                              "'$vertiefung EtIt'  -  Rolle für Vertiefung Elektrotechnik-Informationstechnik" + os.linesep +
+                              "'$vertiefung Info'  -  Rolle für Vertiefung Informatik" + os.linesep +
+                              "'$vertiefung MB'  -  Rolle für Vertiefung Maschinenbau" + os.linesep +
+                              "'$vertiefung MM'  -  Rolle für Vertiefung Mathe/Mechanik" + os.linesep +
+                              "'$vertiefung SV'  -  Rolle für Vertiefung Strömung-Verbrennung" + os.linesep +
+                              "'$vertiefung Rob'  -  Rolle für Vertiefung Computational_Robotics",
+                        inline=False)
+        embed.add_field(name="Andere Befehle für Rollen",
+                        value="'$gast'  -  Rolle für Gast" + os.linesep +
+                              "'$gesellschaftsspiele'  -  Rolle für Gesellschaftsspieler" + os.linesep +
+                              "'$gaming'  -  Rolle für Gaming Interessierte",
+                        inline=False)
+        if channel is not None:
+            await channel.send(embed=embed)
+        else:
+            await ctx.send(embed=embed)
+
+    @commands.command()
     async def hilfe(self, ctx):
-        msg="""Derzeit verfügbare Rollen:
+        msg = """Derzeit verfügbare Rollen:
             Per Bot:
                 `$ersti`
                 `$ersti Master`
@@ -33,6 +85,27 @@ class Help(commands.Cog):
                 Gaming
                 Gesellschaftsspiele"""
         await ctx.send(msg)
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_any_role('Fachschaft', 'Team')
+    async def send_embed(self, ctx, title, text, channel: discord.TextChannel = None, url=None):
+        if url is not None:
+            embed = discord.Embed(title=title, url=url,
+                                  description=text, # " ".join(text)
+                                  color=0x3572AC)
+        else:
+            embed = discord.Embed(title=title,
+                                  description=text,
+                                  color=0x3572AC)
+        if discord.utils.get(ctx.guild.roles, name='Fachschaft') in ctx.author.roles:
+            embed.set_author(name="Fachschaft",
+                             url="https://www.ce.tu-darmstadt.de/ce/studienbereich_ce/organisation_2/fachschaft_ce/index.de.jsp",
+                             icon_url=ctx.guild.icon_url)
+        if channel is not None:
+            await channel.send(embed=embed)
+        else:
+            await ctx.send(embed=embed)
 
 
 def setup(client):
